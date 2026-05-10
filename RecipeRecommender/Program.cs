553e88c598872ec007bfe0cb07f409d6dda0bfe9
Anmol <anmolkumar2633@gmail.com>
2026-05-10 18:53:21 +0530

    Updated Program.cs for Render

--- RecipeRecommender/Program.cs
+++ RecipeRecommender/Program.cs
@@ -4,19 +4,19 @@ using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Controllers + Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+// Services
 builder.Services.AddScoped<IRecipeService, RecipeService>();
 builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
 builder.Services.AddScoped<RedisCacheService>();
 builder.Services.AddHttpClient<SpoonacularService>();
 builder.Services.AddHttpClient<AiService>();
 
-
+// Redis 
 builder.Services.AddSingleton<IConnectionMultiplexer?>(sp =>
 {
     try
@@ -35,7 +35,7 @@ builder.Services.AddSingleton<IConnectionMultiplexer?>(sp =>
     }
 });
 
-
+// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -47,19 +47,26 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowAll");
 
-app.UseHttpsRedirection();
+
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
 
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+
+
+
+app.MapGet("/", () => "Recipe API is running 🚀");
+
+app.Run();
